8fe2c0ac6e3f12a2fce8febdbd74439db1a1029e
Hossam Atef <hossam9k@gmail.com>
2024-11-01 01:23:30 +0200

    added CoroutineModule.kt

---
+++ core/data/src/main/java/com/poc/data/di/CoroutineModule.kt
@@ -0,0 +1,32 @@
+package com.poc.data.di
+
+import com.poc.data.constants.DISPATCHER_DEFAULT_TAG
+import com.poc.data.constants.DISPATCHER_IO_TAG
+import com.poc.data.constants.DISPATCHER_MAIN_TAG
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CoroutineModule {
+    @Provides
+    @Singleton
+    @Named(DISPATCHER_MAIN_TAG)
+    fun provideMainCoroutineDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @Provides
+    @Singleton
+    @Named(DISPATCHER_DEFAULT_TAG)
+    fun provideDefaultCoroutineDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @Singleton
+    @Named(DISPATCHER_IO_TAG)
+    fun provideIoCoroutineDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
--- features/login/src/main/java/com/poc/login/data/di/NetworkModule.kt
+++ features/login/src/main/java/com/poc/login/data/di/NetworkModule.kt
@@ -2,16 +2,20 @@ package com.poc.login.data.di
 
 import com.google.gson.Gson
 import com.poc.data.connectivity.NetworkMonitorInterface
+import com.poc.data.constants.DISPATCHER_DEFAULT_TAG
 import com.poc.data.constants.USER_ID_TAG
 import com.poc.data.factory.ServiceFactory
 import com.poc.data.source.NetworkDataSource
 import com.poc.login.data.service.LoginService
 import com.poc.login.data.source.LoginRemote
 import com.poc.login.data.source.LoginRemoteImpl
+import com.poc.login.domain.mapper.LoginMapper
+import com.poc.login.domain.mapper.LoginMapperImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -43,13 +47,18 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun provideNetworkRemoteImpl(networkDataSource: NetworkDataSource<LoginService>): LoginRemote {
-        return LoginRemoteImpl(networkDataSource)
+    fun provideLoginMapper(
+        @Named(DISPATCHER_DEFAULT_TAG) coroutineDispatcher: CoroutineDispatcher,
+    ): LoginMapper {
+        return LoginMapperImpl(coroutineDispatcher)
     }
 
     @Provides
     @Singleton
-    fun provideLoginRemoteImpl(networkDataSource: NetworkDataSource<LoginService>): LoginRemote{
-        return LoginRemoteImpl(networkDataSource)
+    fun provideLoginRemoteImpl(
+        networkDataSource: NetworkDataSource<LoginService>,
+        loginMapper: LoginMapper,
+    ): LoginRemote {
+        return LoginRemoteImpl(networkDataSource, loginMapper)
     }
 }
